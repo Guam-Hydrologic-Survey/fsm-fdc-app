17014b586de525d3f1adeaadd8cd74fdf4bddb10
Dannika <71364766+dannikate@users.noreply.github.com>
2025-04-02 13:38:43 +1000

    update vars for json paths

--- src/utils/dataSource.js
+++ src/utils/dataSource.js
@@ -7,4 +7,17 @@ Return:
 // update this with data set; must be full path, starting from the root directory 
 const geoJsonUrl = "./src/data/tests/dummy_data.json";
 
-export { geoJsonUrl }
+const base = './src/data/';
+
+const kosraeData = {
+    gages: base + '/kosrae/KOSRAE GAGES WGS84 UTM.json',
+    rivers: base + '/kosrae/KOSRAE RIVERS WGS84 UTM 2.json',
+};
+
+const pohnpeiData = {
+    roads: base + '/pohnpei/POHNPEI_RDS_UTM.json',
+    streams: base + '/pohnpei/STREAMS.json',
+    gages: base + '/pohnpei/USGS_GAGES.json',
+}
+
+export { geoJsonUrl, kosraeData, pohnpeiData }
