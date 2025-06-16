bcb8a93f4ba8b65b51bde44afca71e824c5b0802
Dannika <71364766+dannikate@users.noreply.github.com>
2025-06-16 10:09:44 +1000

    update paths

--- src/utils/dataSource.js
+++ src/utils/dataSource.js
@@ -10,14 +10,14 @@ const geoJsonUrl = "./src/data/tests/dummy_data.json";
 const base = './src/data/';
 
 const kosraeData = {
-    gages: base + '/kosrae/KOSRAE GAGES WGS84 UTM.json',
-    rivers: base + '/kosrae/KOSRAE RIVERS WGS84 UTM 2.json',
+    gages: base + 'kosrae/KOSRAE GAGES WGS84 UTM.json',
+    rivers: base + 'kosrae/KOSRAE RIVERS WGS84 UTM 2.json',
 };
 
 const pohnpeiData = {
-    roads: base + '/pohnpei/POHNPEI_RDS_UTM.json',
-    streams: base + '/pohnpei/POHNPEI STREAMS.json',
-    gages: base + '/pohnpei/POHNPEI USGS_GAGES.json',
+    roads: base + 'pohnpei/POHNPEI_RDS_UTM.json',
+    streams: base + 'pohnpei/POHNPEI STREAMS.json',
+    gages: base + 'pohnpei/POHNPEI USGS_GAGES.json',
 }
 
 export { geoJsonUrl, kosraeData, pohnpeiData }
